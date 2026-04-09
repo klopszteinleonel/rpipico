d8b0a881b237a90af6e4ad032ada6ad1acd2fc4f
Leonel <klopszteinleonel@gmail.com>
2026-04-09 15:28:59 -0300

    escribe mac

--- main.py
+++ main.py
@@ -1,70 +1,12 @@
-# (C) Copyright Peter Hinch 2017-2019.
-# Released under the MIT licence.
+import network
+import ubinascii
 
-# This demo publishes to topic "result" and also subscribes to that topic.
-# This demonstrates bidirectional TLS communication.
-# You can also run the following on a PC to verify:
-# mosquitto_sub -h test.mosquitto.org -t result
-# To get mosquitto_sub to use a secure connection use this, offered by @gmrza:
-# mosquitto_sub -h <my local mosquitto server> -t result -u <username> -P <password> -p 8883
+# Activamos la interfaz de red
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
 
-# Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
+# Sacamos la MAC en bytes y la pasamos a un formato legible
+mac_raw = wlan.config('mac')
+mac_address = ubinascii.hexlify(mac_raw, ':').decode().upper()
 
-# red LED: ON == WiFi fail
-# green LED heartbeat: demonstrates scheduler is running.
-
-from mqtt_as import MQTTClient
-from mqtt_local import config
-import uasyncio as asyncio
-import dht, machine
-
-d = dht.DHT11(machine.Pin(15))
-
-async def messages(client):  # Respond to incoming messages
-    async for topic, msg, retained in client.queue:
-        print((topic, msg, retained))
-
-async def up(client):  # Respond to connectivity being (re)established
-    while True:
-        await client.up.wait()  # Wait on an Event
-        client.up.clear()
-        await client.subscribe('foo_topic', 1)  # renew subscriptions
-
-
-async def main(client):
-    await client.connect()
-
-    for coroutine in (up, messages):
-        asyncio.create_task(coroutine(client))
-    
-    n = 0
-    await asyncio.sleep(3)  # Give broker time
-    while True:
-        try:
-            d.measure()
-            try:
-                temperatura=d.temperature()
-                await client.publish('Leo/temperatura', '{}'.format(temperatura), qos = 1)
-            except OSError as e:
-                print("sin sensor temperatura")
-            try:
-                humedad=d.humidity()
-                await client.publish('Leo/humedad', '{}'.format(humedad), qos = 1)
-            except OSError as e:
-                print("sin sensor humedad")
-        except OSError as e:
-            print("sin sensor")
-        await asyncio.sleep(20)  # Broker is slow
-
-# Define configuration
-config['ssl'] = True
-config["queue_len"] = 1  # Use event interface with default queue size
-
-# Set up client
-MQTTClient.DEBUG = True  # Optional
-client = MQTTClient(config)
-try:
-    asyncio.run(main(client))
-finally:
-    client.close()
-    asyncio.new_event_loop()
+print(f"MAC Address: {mac_address}")
