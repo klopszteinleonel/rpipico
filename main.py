d4c8fb9e5f951130dc8215b2fb74e79eab072377
Leonel <klopszteinleonel@gmail.com>
2026-04-14 16:51:12 -0300

    recibe datos, actualiza variables y las guarda en flash, lee de flash e implementa logica del rele, soluciona ussl en la libreria mqtt_as

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ from mqtt_local import config
 import uasyncio as asyncio
 import dht, machine
 import ujson
+import os
 import network
 import ubinascii
 from led_async import LED_async
+from machine import Pin
 
 # 1. Obtenemos la MAC para el tópico (dinámico)
 wlan = network.WLAN(network.STA_IF)
@@ -34,16 +36,111 @@ periodo = 5
 modos = ['AUTO', 'MAN']
 modo = modos[0]
 led_board = Pin("LED", Pin.OUT)
-led = LED_async(led_board)
+led_board.off()
 
+rele = Pin(16, Pin.OUT)
+rele.on()
+
+DB_FILE = "config.json"
+
+# Función para cargar los datos al arrancar
+def cargar_db():
+    try:
+        with open(DB_FILE, "r") as f:
+            return ujson.load(f)
+    except (OSError, ValueError):
+        # Si el archivo no existe o está roto, devolvemos un dict vacío
+        return {}
+
+# Función para guardar un dato
+def guardar_dato(clave, valor):
+    db = cargar_db()
+    db[clave] = valor
+    with open(DB_FILE, "w") as f:
+        ujson.dump(db, f)
+
+def cargar_config_inicial():
+    global setpoint, modo, periodo
+    
+    print("Cargando configuración desde la memoria...")
+    datos = cargar_db() # Usamos la función que lee el JSON
+    
+    if datos:
+        # Usamos .get(llave, default) para evitar que el código explote si falta una llave
+        setpoint = float(datos.get("setpoint", setpoint))
+        modo = str(datos.get("modo", modo))
+        periodo = int(datos.get("periodo", periodo))
+        
+        print("Configuración cargada con éxito:")
+        print(f" -> Setpoint: {setpoint}")
+        print(f" -> Modo: {modo}")
+        print(f" -> Periodo: {periodo}")
+    else:
+        print("No se encontró archivo de configuración. Usando valores por defecto.")
+
+def es_numero(v):
+    try:
+        float(v)
+        return True
+    except ValueError:
+        return False
 
 async def messages(client):  # Respond to incoming messages
     async for topic, msg, retained in client.queue:
+        global setpoint, periodo, modos, modo
 
         if(topic.decode() == f'{mac_id}/destello'):
-            led.flash(0.7 + n/4)
+            led_board.on()
+            await asyncio.sleep_ms(300)
+            led_board.off()
+            await asyncio.sleep_ms(300)
+            led_board.on()
+            await asyncio.sleep_ms(300)
+            led_board.off()
         
-        print(f"Guardado -> Topico: {topic.decode()}, Valor: {msg.decode()}")
+        if(topic.decode() == f'{mac_id}/setpoint'):
+            m = msg.decode()
+            if(es_numero(m)):
+                val = float(m)
+                guardar_dato("setpoint", val)
+                setpoint = val
+                print(f"Guardado -> Topico: {topic.decode()}, Valor: {val}")
+            else: 
+                print("Variable no es numero")
+
+        if(topic.decode() == f'{mac_id}/periodo'):
+            m = msg.decode()
+            if(es_numero(m)):
+                val = float(m)
+                guardar_dato("periodo", val)
+                periodo = val
+                print(f"Guardado -> Topico: {topic.decode()}, Valor: {val}")
+                print(f"Periodo: {periodo}")
+            else: 
+                print("Variable no es numero")
+
+        if(topic.decode() == f'{mac_id}/modo'):
+            m = msg.decode()
+            if(m == 'AUTO'):
+                guardar_dato("modo", modos[0])
+                modo = modos[0]
+                print(f"Guardado -> Topico: {topic.decode()}, Valor: {modos[0]}")
+            elif(m == 'MAN'):
+                guardar_dato("modo", modos[1])
+                modo = modos[1]
+                print(f"Guardado -> Topico: {topic.decode()}, Valor: {modos[1]}")
+            else: 
+                print("Variable no es modo (AUTO, MAN)")
+
+        if(topic.decode() == f'{mac_id}/rele'):
+            m = msg.decode()
+            if(modo == 'MAN'):
+                rele.toggle()
+                print(f"Se accionó el relé")
+            else: 
+                print("Modo AUTO")
+                
+        
 
 async def up(client):  # Respond to connectivity being (re)established
     while True:
@@ -71,6 +168,11 @@ async def main(client):
 
                 temperatura=d.temperature()
 
+                if(temperatura > setpoint):
+                    rele.off()
+                elif(temperatura < setpoint - 2): #pequeño offset
+                    rele.on()
+                
                 # 2. Armamos el diccionario con los datos
                 datos = {
                     'temp': d.temperature(),
@@ -99,6 +201,7 @@ config['queue_len'] = 1  # Use event interface with default queue size
 # Set up client
 MQTTClient.DEBUG = True  # Optional
 client = MQTTClient(config)
+cargar_config_inicial()
 try:
     asyncio.run(main(client))
 finally:
